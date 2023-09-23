bcfdb13a1fa37befe3ef16ff02f18bdf0af3678c
Jake <73198594+jaborsh@users.noreply.github.com>
2023-09-22 19:14:05 -0600

    Added directional says.

--- commands/general.py
+++ commands/general.py
@@ -17,6 +17,7 @@ __all__ = [
     "CmdGive",
     "CmdInventory",
     "CmdLook",
+    "CmdSay",
     "CmdTell",
 ]
 
@@ -399,6 +400,54 @@ class CmdLook(default_general.CmdLook):
 
     Observes your location or objects in your vicinity.
     """
+
+
+class CmdSay(Command):
+    """
+    Usage: say <message>
+           say to <character> <message>
+           say to <character>,[character,...] <message>
+
+    Example: say to jake,john Hi there!
+
+    Talk to those in your current location or directly to specific people
+    nearby.
+    """
+
+    key = "say"
+    aliases = ['"', "'"]
+    locks = "cmd:all()"
+    arg_regex = None
+
+    def func(self):
+        caller = self.caller
+        args = self.args.strip()
+        receivers = []
+        if not args:
+            caller.msg("Say what?")
+            return
+
+        speech = args
+        if args.startswith("to "):
+            for obj in args.split(" ", 2)[1].split(","):
+                receiver = caller.search(obj)
+                if not receiver:
+                    continue
+                receivers.append(receiver)
+
+            if not receivers:
+                return caller.msg("Who do you want to talk to?")
+            try:
+                speech = args.split(" ", 2)[2]
+            except IndexError:
+                return caller.msg("Have you forgotten what you'd like to say to them?")
+
+        speech = caller.at_pre_say(speech)
+
+        if not speech:
+            return
+
+        caller.at_say(speech, msg_self=True, receivers=receivers or None)
 
 
 class CmdTell(Command):
--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -54,6 +54,200 @@ class Character(ObjectParent, DefaultCharacter):
     def log_folder(self):
         return self.attributes.get("_log_folder", f"characters/{self.key.lower()}/")
 
+    def at_say(
+        self,
+        message,
+        msg_self=None,
+        msg_location=None,
+        receivers=None,
+        msg_receivers=None,
+        **kwargs,
+    ):
+        """
+        Display the actual say (or whisper) of self.
+
+        This hook should display the actual say/whisper of the object in its
+        location.  It should both alert the object (self) and its
+        location that some text is spoken.  The overriding of messages or
+        `mapping` allows for simple customization of the hook without
+        re-writing it completely.
+
+        Args:
+            message (str): The message to convey.
+            msg_self (bool or str, optional): If boolean True, echo `message` to self. If a string,
+                return that message. If False or unset, don't echo to self.
+            msg_location (str, optional): The message to echo to self's location.
+            receivers (Object or iterable, optional): An eventual receiver or receivers of the
+                message (by default only used by whispers).
+            msg_receivers(str): Specific message to pass to the receiver(s). This will parsed
+                with the {receiver} placeholder replaced with the given receiver.
+        Keyword Args:
+            whisper (bool): If this is a whisper rather than a say. Kwargs
+                can be used by other verbal commands in a similar way.
+            mapping (dict): Pass an additional mapping to the message.
+
+        Notes:
+
+
+            Messages can contain {} markers. These are substituted against the values
+            passed in the `mapping` argument.
+
+                msg_self = 'You say: "{speech}"'
+                msg_location = '{object} says: "{speech}"'
+                msg_receivers = '{object} whispers: "{speech}"'
+
+            Supported markers by default:
+                {self}: text to self-reference with (default 'You')
+                {speech}: the text spoken/whispered by self.
+                {object}: the object speaking.
+                {receiver}: replaced with a single receiver only for strings meant for a specific
+                    receiver (otherwise 'None').
+                {all_receivers}: comma-separated list of all receivers,
+                                 if more than one, otherwise same as receiver
+                {location}: the location where object is.
+
+        """
+
+        def format_receivers(receiver, receivers, type, self_obj):
+            """Format the receivers' list into a readable string."""
+            if type == "self":
+                display_names = [recv.get_display_name(self_obj) for recv in receivers]
+            elif type == "receiver":
+                display_names = [
+                    "you"
+                    if character == receiver
+                    else character.get_display_name(receiver)
+                    for character in receivers
+                ]
+            else:
+                display_names = [recv.get_display_name(receiver) for recv in receivers]
+
+            if len(display_names) > 2:
+                return (
+                    ", ".join(display_names[:-2])
+                    + ", and "
+                    + " and ".join(display_names[-2:])
+                )
+            elif len(display_names) == 2:
+                return " and ".join(display_names)
+            elif display_names:
+                return display_names[0]
+            return ""
+
+        def construct_self_message(
+            self_obj, receivers, location, message, msg_self, msg_type, custom_mapping
+        ):
+            all_receivers = format_receivers(self_obj, receivers, "self", self_obj)
+            self_mapping = {
+                "self": "You",
+                "to": " to " if receivers else "",
+                "object": self_obj.get_display_name(self_obj),
+                "location": location.get_display_name(self_obj) if location else None,
+                "receiver": None,
+                "all_receivers": all_receivers,
+                "speech": message,
+            }
+            self_mapping.update(custom_mapping)
+            self_obj.msg(
+                text=(msg_self.format_map(self_mapping), {"type": msg_type}),
+                from_obj=self_obj,
+            )
+
+        def construct_receiver_messages(
+            self_obj, receivers, location, message, msg_receivers, msg_type
+        ):
+            for receiver in receivers:
+                all_receivers = format_receivers(
+                    receiver, receivers, "receiver", self_obj
+                )
+                receiver_mapping = {
+                    "self": "You",
+                    "to": " to " if receivers else "",
+                    "object": self_obj.get_display_name(receiver),
+                    "location": location.get_display_name(receiver)
+                    if location
+                    else None,
+                    "receiver": None,
+                    "all_receivers": all_receivers,
+                    "speech": message,
+                }
+                receiver.msg(
+                    text=(
+                        msg_receivers.format_map(receiver_mapping),
+                        {"type": msg_type},
+                    ),
+                    from_obj=self_obj,
+                )
+
+        def construct_location_message(
+            self_obj,
+            receivers,
+            location,
+            message,
+            msg_location,
+            msg_type,
+            custom_mapping,
+        ):
+            exclude = [self_obj]
+            if receivers:
+                exclude.extend(receivers)
+
+            for individual in location.contents:
+                if individual in exclude:
+                    continue
+                all_receivers = format_receivers(
+                    individual, receivers, "location", self_obj
+                )
+                location_mapping = {
+                    "self": "You",
+                    "object": self_obj,
+                    "to": " to " if receivers else "",
+                    "location": location,
+                    "all_receivers": all_receivers,
+                    "receiver": None,
+                    "speech": message,
+                }
+                location_mapping.update(custom_mapping)
+                individual.msg(
+                    text=(
+                        msg_location.format_map(location_mapping),
+                        {"type": msg_type},
+                    ),
+                    from_obj=self_obj,
+                )
+
+        msg_type = "say"
+        msg_self = (
+            '{self} say{to}{all_receivers}, "|n{speech}|n"'
+            if msg_self is True
+            else msg_self
+        )
+        msg_location = msg_location or '{object} says{to}{all_receivers}, "{speech}"'
+        msg_receivers = msg_receivers or '{object} says{to}{all_receivers}, "{speech}"'
+
+        custom_mapping = kwargs.get("mapping", {})
+        receivers = make_iter(receivers) if receivers else []
+        location = self.location
+
+        if msg_self:
+            construct_self_message(
+                self, receivers, location, message, msg_self, msg_type, custom_mapping
+            )
+        if receivers and msg_receivers:
+            construct_receiver_messages(
+                self, receivers, location, message, msg_receivers, msg_type
+            )
+        if location and msg_location:
+            construct_location_message(
+                self,
+                receivers,
+                location,
+                message,
+                msg_location,
+                msg_type,
+                custom_mapping,
+            )
+
     def msg(self, text=None, from_obj=None, session=None, options=None, **kwargs):
         """
         Emits something to a session attached to the object.
