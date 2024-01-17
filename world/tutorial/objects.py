0fa9011e28c0f8f713e1ed9cf986303b99405cf6
= <=>
2024-01-16 20:12:44 -0700

    Tutorial Progress

---
+++ world/tutorial/objects.py
@@ -0,0 +1,52 @@
+class Figure:  # (1,2)
+    desc = "The figure is a tragic centerpiece amidst the room's cartographic splendor. Seated with their cranium exposed to the whims of cruel fate, their visage is frozen in an eerie semblance of ponderous thought. The soft, vulnerable tissue of the brain is a stark contrast to the rigid discipline of the maps that encircle them. Traces of blood, now dry and darkened, have painted rivulets along their weathered skin - a macabre depiction of the body's fragility."
+    feel = "A visceral discomfort arises at the sight, a tension that coils in the pit of one's being, as if in the presence of nature's uncanny aberration."
+    smell = "The metallic pungency of blood intermingles with the sterile scent of exposed cerebral matter."
+    sound = "An unsettling silence emanates from the figure, their breaths having long since yielded to the quietude that now claims the air."
+    taste = "Climically bitter, the mind's flesh exudes an intangible flavor that speaks to innermost sanctums violated."
+
+
+class GoblinCorpse:  # (2,2)
+    desc = "A lone goblin figure lies crumbled upon the cold floor, their once deft hands now stilled by irreversible decree. Clad in the intricate garb of high ranking kin, the corpse seems almost in repose, as if they lay down willingly."
+    feel = "A chill emanates from the goblin's final resting place, as if they very air mourns the loss of the curious intellect that once inhabited the small, wiry frame."
+    smell = "The scient of the corpse has been cleansed by the briny environment, leaving only the faintest trace of the sharp, earthy odors that are characteristic of goblinkind."
+    sound = "The stillness enveloping the figure is profound, the absence of their characteristic chatter amplifying the oppressive silence that has befallen the map room."
+    taste = "A subtle taste of iron lingers in the air, an echo of the lifeblood that once coursed with vigor through the goblin's industrious heart."
+
+
+class Scimitar:  # (1,3)
+    pass
+
+
+class Handaxe:  # (1,3)
+    pass
+
+
+class Crossbow:  # (1,3)
+    pass
+
+
+class ShortSword:  # (1,3)
+    pass
+
+
+class HealingPotion:  # (1,3)
+    pass
+
+
+class ImpCorpse:  # (1,3)
+    # has handaxe and crossbow
+    pass
+
+
+class ThrallCorpse:  # (1,3)
+    # has shortsword, simple robe, and potion of healing
+    pass
+
+
+class Cultist:
+    desc = "A menacing figure stands apart from the desolation, a stark anomaly in the oppressive gloom. The cultist, draped in tattered robes that hang from their frame like the shrouds of the fallen, mutter incantations in a chilling, discordant tone. Their face is obscured by a cowl from which the glint of their eyes pierce the murk, alight with fervor for an unfathomable rite. With measured intent, they move, hands traced with symbols that seem to writhe and pulse with a life of their own upon their flesh - a dark communition etched into their very being."
+    feel = "The cultist exudes a malevolent chill, a menacing draft of rituals best left unspoken."
+    smell = "An underlying scent of bitter herbs and the coppery essence of spilled lifeblood is carried faintly on their wake, filling the hold with an unsettling aroma."
+    sound = "The soft drone of their low chant reverberates off the stone and iron, an ominous rhythm that seeps into the stillness."
+    taste = "The taste of dread hangs sharp in the air as something sinister unfolds with each of their uttered syllables."
