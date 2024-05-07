c53e7feff983dc1b4f7e5b26ad7bb480d75375cd
Jake <jaborsh@users.noreply.github.com>
2024-05-06 21:34:22 -0600

    Fixing more bugs related to chargen/Nautilus

--- world/nautilus/update.py
+++ world/nautilus/update.py
@@ -1,6 +1,5 @@
 from evennia.prototypes import spawner
-
 from world.nautilus.prototypes import PROTOTYPES
 
 for prototype in PROTOTYPES:
-    spawner.batch_update_objects_with_prototype(prototype)
+    objs = spawner.batch_update_objects_with_prototype(prototype)
