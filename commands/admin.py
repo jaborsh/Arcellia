ec544d16284dace4d55f23f7f37bbeeff37161c6
Jake <jaborsh@users.noreply.github.com>
2024-07-06 23:40:28 -0700

    Fixing CmdWatch second bug (recursive watch)

--- commands/admin.py
+++ commands/admin.py
@@ -557,6 +557,9 @@ class CmdWatch(Command):
         elif caller.ndb._watching == target:
             self.msg(f"You are already watching {target.name}.")
             return
+        elif target.ndb._watching == caller:
+            self.msg(f"{target.name} is already watching you.")
+            return
 
         if caller.ndb._watching:
             self._stop_watching(caller)
