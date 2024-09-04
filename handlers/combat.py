4bd58255888bee0b1119e1c32179576f6a0bbea1
Jake <=>
2024-09-04 13:46:55 -0600

    feat: Rewriting the framework for combat. Currently, no actual actions taken.

--- handlers/combat.py
+++ handlers/combat.py
@@ -1,106 +1,124 @@
-from collections import deque
-from enum import Enum, auto
-
 from .handler import Handler
 
 
-class TurnState(Enum):
-    WAITING = auto()
-    DONE = auto()
-
-
-class PlayerCombatHandler(Handler):
-    def __init__(self, obj, db_attribute_key, db_attribute_category=None):
-        super().__init__(obj, db_attribute_key, db_attribute_category, default_data={})
-
-
 class CombatHandler(Handler):
-    def __init__(self, obj, db_attribute_key, db_attribute_category=None):
-        """Initialize combat handler with reference to the parent object."""
-        super().__init__(obj, db_attribute_key, db_attribute_category, default_data={})
-        self.combatants = self._data
-        self.turn_queue = deque()
+    """
+    A class to handle combat-related data for an object, particularly managing combatants and their enemies.
 
-    def _update_turn_queue(self):
-        """Update the turn order based on combatant dexterity and state."""
-        sorted_combatants = sorted(
-            self.combatants.keys(),
-            key=lambda combatant: (
-                self.combatants[combatant]["state"] == TurnState.WAITING,
-                -combatant.stats.get("dexterity").value,
-            ),
-        )
+    Inherits from:
+        Handler: A general-purpose data handler.
 
-        self.turn_queue = deque(sorted_combatants)
+    Attributes:
+        combatants (dict): A dictionary where the key is a combatant, and the value is a list of their enemies.
+    """
 
-    def add_combatant(self, combatant, enemies):
-        """Add a new combatant to the handler if not already present.
-
-        Args:
-            combatant: An instance representing a character or creature.
+    def __init__(
+        self,
+        obj,
+        db_attribute_key="combat",
+        db_attribute_category=None,
+        default_data=None,
+    ):
         """
-        if combatant not in self.combatants:
-            self.combatants[combatant] = {"enemies": set(), "state": TurnState.WAITING}
-            combatant.ndb.combat_action = None
+        Initializes the CombatHandler object.
 
-        self.add_enemy_relationship(combatant, enemies)
-        self._update_turn_queue()
-        self._save()
+        Parameters:
+            obj (object): The object for which combat data is being handled.
+            db_attribute_key (str): The key under which combat data is stored.
+            db_attribute_category (str, optional): The category of the attribute in the object's attributes dictionary.
+            default_data (dict, optional): Default data to use for initialization, if none exists.
+        """
+        if default_data is None:
+            default_data = {"combatants": {}}
+        super().__init__(obj, db_attribute_key, db_attribute_category, default_data)
 
-    def add_enemy_relationship(self, combatant, enemies):
-        if not isinstance(enemies, (list, set)):
+    def add_combat(self, combatant, enemies=None):
+        """
+        Adds a combatant and their enemies to the combatants dictionary.
+
+        If the combatant does not exist, they are added. Enemies are then added to their enemy list.
+
+        Parameters:
+            combatant (str): The combatant to be added.
+            enemies (object or list): The enemies to be added to the combatant's list of enemies. Can be a single enemy or a list of enemies.
+        """
+
+        if not enemies:
+            return
+
+        if not isinstance(enemies, list):
             enemies = [enemies]
 
-        for enemy in enemies:
-            self.combatants[combatant]["enemies"].add(enemy)
+        self._data["combatants"].setdefault(combatant, [])
 
-            if enemy not in self.combatants:
-                self.combatants[enemy] = {"enemies": set(), "state": TurnState.WAITING}
-                enemy.ndb.combat_action = None
+        # Add only new enemies to avoid duplicates
+        existing_enemies = set(self._data["combatants"][combatant])
+        new_enemies = set(enemies) - existing_enemies
 
-            self.combatants[enemy]["enemies"].add(combatant)
+        if new_enemies:
+            self._data["combatants"][combatant].extend(new_enemies)
+
+            for enemy in new_enemies:
+                self._data["combatants"].setdefault(enemy, [])
+                if combatant not in self._data["combatants"][enemy]:
+                    self._data["combatants"][enemy].append(combatant)
+
+        self._save()
 
     def remove_combatant(self, combatant):
-        """Remove a combatant from the handler.
-
-        Args:
-            combatant: An instance representing a character or creature.
         """
-        if combatant in self.combatants:
-            del self.combatants[combatant]
-            if combatant in self.turn_queue:
-                self.turn_queue.remove(combatant)
+        Removes a combatant from the combatants list and from other combatants' enemy lists.
 
-            # Create a list of keys to safely remove elements during iteration
-            for other_combatant in list(self.combatants):
-                self.combatants[other_combatant]["enemies"].discard(combatant)
-                if not self.combatants[other_combatant]["enemies"]:
-                    del self.combatants[other_combatant]
+        Parameters:
+            combatant (str): The combatant to be removed.
+        """
+        if combatant in self._data["combatants"]:
+            del self._data["combatants"][combatant]
 
-        self._update_turn_queue()
-        self._save()
+            # Remove combatant from all enemy lists
+            for k in list(self._data["combatants"].keys()):
+                v = self._data["combatants"][k]
+                if combatant in v:
+                    self._data["combatants"][k] = [e for e in v if e != combatant]
 
-    def start_round(self):
-        """Start a new round of combat."""
-        for combatant in self.combatants:
-            self.combatants[combatant]["state"] = TurnState.WAITING
+                if not self._data["combatants"][k]:
+                    del self._data["combatants"][k]
 
-        self._update_turn_queue()
-        self._save()
+            self._save()
 
-        while self.turn_queue:
-            combatant = self.turn_queue.popleft()
-            self.execute_turn(combatant)
+    def remove_enemy(self, combatant, enemy):
+        """
+        Removes an enemy from a specific combatant's enemy list.
 
-        self.end_round()
+        Parameters:
+            combatant (str): The combatant whose enemy will be removed.
+            enemy (str): The enemy to be removed from the combatant's enemy list.
 
-    def execute_turn(self, combatant):
-        print(f"{combatant.key} attacks!")
-        self.combatants[combatant]["state"] = TurnState.DONE
+        Returns:
+            None
+        """
+        if combatant in self._data["combatants"]:
+            if enemy in self._data["combatants"][combatant]:
+                self._data["combatants"][combatant].remove(enemy)
+                self._save()
 
-    def end_round(self):
-        """End the current round of combat."""
-        print("The round has ended.")
+    def get_enemies(self, combatant):
+        """
+        Returns the list of enemies for a given combatant.
 
-    def end_combat(self):
-        pass
+        Parameters:
+            combatant (str): The combatant whose enemies are requested.
+
+        Returns:
+            list: A list of enemies for the specified combatant.
+        """
+        return self._data["combatants"].get(combatant, [])
+
+    def all_combatants(self):
+        """
+        Returns all combatants in the combatants dictionary.
+
+        Returns:
+            list: A list of all combatants.
+        """
+        return list(self._data["combatants"].keys())
--- handlers/testing/test_combat.py
+++ handlers/testing/test_combat.py
@@ -1,52 +1,77 @@
-from collections import deque
-from unittest.mock import call, patch
-
 from evennia import create_object
 from evennia.utils.test_resources import EvenniaCommandTest
-from handlers.combat import TurnState
 
 
 class TestCombatHandler(EvenniaCommandTest):
     def setUp(self):
         super().setUp()
-        self.mob = create_object("typeclasses.mobs.Mob", key="Mob")
-        self.mob.dexterity.base = 11
-        self.room1.combat.add_combatant(self.char1, self.mob)
+        self.mob1 = create_object("typeclasses.mobs.Mob", key="Mob")
+        self.mob2 = create_object("typeclasses.mobs.Mob", key="Mob2")
 
-    def test_combat_initialization(self):
+    def test_combat_default_initialization(self):
+        self.assertEqual(self.room1.combat._data, {"combatants": {}})
+
+    def test_adding_combatant_with_no_enemies(self):
+        self.room1.combat.add_combat(self.char1)
+        self.assertEqual(self.room1.combat._data, {"combatants": {}})
+
+    def test_add_new_combatant_successfully(self):
+        self.room1.combat.add_combat(self.char1, self.mob1)
+        self.assertIn(self.char1, self.room1.combat._data["combatants"])
+        self.assertIn(self.mob1, self.room1.combat._data["combatants"])
+        self.assertIn(self.mob1, self.room1.combat._data["combatants"][self.char1])
+        self.assertIn(self.char1, self.room1.combat._data["combatants"][self.mob1])
+
+    def test_add_existing_combatant(self):
+        self.room1.combat.add_combat(self.char1, self.mob1)
+        self.room1.combat.add_combat(self.char1, self.mob1)
+        self.assertEqual(self.room1.combat._data["combatants"][self.char1], [self.mob1])
+        self.assertEqual(self.room1.combat._data["combatants"][self.mob1], [self.char1])
+        self.assertEqual(len(self.room1.combat._data["combatants"]), 2)
+
+    def test_adding_combatant_with_list_of_enemies(self):
+        self.room1.combat.add_combat(self.char1, [self.mob1, self.mob2])
         self.assertEqual(
-            self.room1.combat.combatants,
-            {
-                self.char1: {"enemies": {self.mob}, "state": TurnState.WAITING},
-                self.mob: {"enemies": {self.char1}, "state": TurnState.WAITING},
-            },
+            self.room1.combat._data["combatants"][self.char1], [self.mob1, self.mob2]
         )
-        self.assertEqual(self.room1.combat.turn_queue, deque([self.mob, self.char1]))
+        self.assertEqual(len(self.room1.combat._data["combatants"]), 3)
 
-    @patch("builtins.print")
-    def test_execute_combatant_turn(self, mock_print):
-        self.room1.combat.execute_turn(self.char1)
+    def test_removing_nonexistent_combatant(self):
+        self.room1.combat.add_combat(self.char1, self.mob1)
+        self.room1.combat.remove_combatant(self.char2)
+        self.assertEqual(self.room1.combat._data["combatants"][self.char1], [self.mob1])
+        self.assertEqual(self.room1.combat._data["combatants"][self.mob1], [self.char1])
 
-        expected_calls = [call("Char attacks!")]
-
-        mock_print.assert_has_calls(expected_calls)
-        self.assertEqual(
-            self.room1.combat.combatants[self.char1]["state"], TurnState.DONE
-        )
-
-    @patch("builtins.print")
-    def test_combat_round(self, mock_print):
-        self.room1.combat.start_round()
-
-        expected_calls = [
-            call("Mob attacks!"),
-            call("Char attacks!"),
-            call("The round has ended."),
-        ]
-
-        mock_print.assert_has_calls(expected_calls, any_order=False)
-
-    def test_remove_combatant(self):
+    def test_removing_combatant_success(self):
+        self.room1.combat.add_combat(self.char1, self.mob1)
         self.room1.combat.remove_combatant(self.char1)
-        self.assertNotIn(self.char1, self.room1.combat.combatants)
-        self.assertNotIn(self.mob, self.room1.combat.combatants)
+        self.assertNotIn(self.char1, self.room1.combat._data["combatants"])
+        self.assertNotIn(self.mob1, self.room1.combat._data["combatants"])
+
+    # @patch("builtins.print")
+    # def test_execute_combatant_turn(self, mock_print):
+    #     self.room1.combat.execute_turn(self.char1)
+
+    #     expected_calls = [call("Char attacks!")]
+
+    #     mock_print.assert_has_calls(expected_calls)
+    #     self.assertEqual(
+    #         self.room1.combat.combatants[self.char1]["state"], TurnState.DONE
+    #     )
+
+    # @patch("builtins.print")
+    # def test_combat_round(self, mock_print):
+    #     self.room1.combat.start_round()
+
+    #     expected_calls = [
+    #         call("Mob attacks!"),
+    #         call("Char attacks!"),
+    #         call("The round has ended."),
+    #     ]
+
+    #     mock_print.assert_has_calls(expected_calls, any_order=False)
+
+    # def test_remove_combatant(self):
+    #     self.room1.combat.remove_combatant(self.char1)
+    #     self.assertNotIn(self.char1, self.room1.combat.combatants)
+    #     self.assertNotIn(self.mob, self.room1.combat.combatants)
