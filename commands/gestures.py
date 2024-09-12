b2bc31b034c47fb7094809455568cafe89b20db0
Jake <jaborsh@users.noreply.github.com>
2024-09-12 17:23:27 -0600

    Refactor gesture command messages for various gestures

--- commands/gestures.py
+++ commands/gestures.py
@@ -15,6 +15,16 @@ class GESTURE(Enum):
 
 
 _GESTURES = {
+    "admire": {
+        GESTURE.DEFAULT: "$You() $conj(admire) the view.",
+        GESTURE.TARGET: "$You() $conj(admire) $you(target).",
+        GESTURE.SELF: "$You() $conj(admire) $pron(yourself).",
+    },
+    "apologize": {
+        GESTURE.DEFAULT: "$You() $conj(apologize).",
+        GESTURE.TARGET: "$You() $conj(apologize) to $you(target).",
+        GESTURE.SELF: "$You() $conj(apologize) to $pron(yourself).",
+    },
     "beckon": {
         GESTURE.DEFAULT: "$You() $conj(beckon).",
         GESTURE.TARGET: "$You() $conj(beckon) to $you(target).",
@@ -24,6 +34,11 @@ _GESTURES = {
         GESTURE.DEFAULT: "$You() $conj(bow).",
         GESTURE.TARGET: "$You() $conj(bow) to $you(target).",
         GESTURE.SELF: "$You() $conj(bow) to $pron(yourself), acknowledging $pron(your) own greatness.",
+    },
+    "brag": {
+        GESTURE.DEFAULT: "$You() $conj(brag).",
+        GESTURE.TARGET: "$You() $conj(brag) about $you(target).",
+        GESTURE.SELF: "$You() $conj(brag) about $pron(yourself).",
     },
     "calm": {
         GESTURE.DEFAULT: "$You() $conj(calm) down.",
@@ -40,10 +55,25 @@ _GESTURES = {
         GESTURE.TARGET: "$You() $conj(cry) at the sight of $you(target).",
         GESTURE.SELF: "$You() $conj(cry) to $pron(yourself), because no one else understands.",
     },
+    "cuddle": {
+        GESTURE.DEFAULT: "$You() $conj(cuddle) yourself.",
+        GESTURE.TARGET: "$You() $conj(cuddle) $you(target).",
+        GESTURE.SELF: "$You() $conj(cuddle) $pron(yourself).",
+    },
     "curtsy": {
         GESTURE.DEFAULT: "$You() $conj(curtsy).",
         GESTURE.TARGET: "$You() $conj(curtsy) towards $you(target).",
         GESTURE.SELF: "$You() $conj(curtsy) to $pron(yourself).",
+    },
+    "dance": {
+        GESTURE.DEFAULT: "$You() $conj(dance).",
+        GESTURE.TARGET: "$You() $conj(dance) with $you(target).",
+        GESTURE.SELF: "$You() $conj(dance) with $pron(yourself).",
+    },
+    "flirt": {
+        GESTURE.DEFAULT: "$You() $conj(flirt).",
+        GESTURE.TARGET: "$You() $conj(flirt) with $you(target).",
+        GESTURE.SELF: "$You() $conj(flirt) with $pron(yourself). So debutante.",
     },
     "gesture": {
         GESTURE.DEFAULT: "$You() $conj(gesture).",
@@ -60,10 +90,25 @@ _GESTURES = {
         GESTURE.TARGET: "$You() $conj(grovel) at $your(target) feet.",
         GESTURE.SELF: "$You() $conj(grovel) before $pron(yourself), because even $pron(you) $pconj(are) not worthy of $pron(your) own respect.",
     },
+    "hug": {
+        GESTURE.DEFAULT: "$You() $conj(hug) yourself.",
+        GESTURE.TARGET: "$You() $conj(hug) $you(target).",
+        GESTURE.SELF: "$You() $conj(hug) $pron(yourself).",
+    },
     "jump": {
         GESTURE.DEFAULT: "$You() $conj(jump).",
         GESTURE.TARGET: "$You() $conj(jump) on $you(target).",
         GESTURE.SELF: "$You() $conj(jump) for $pron(yourself), because self-celebration is important.",
+    },
+    "kiss": {
+        GESTURE.DEFAULT: "$You() $conj(kiss) the air.",
+        GESTURE.TARGET: "$You() $conj(kiss) $you(target).",
+        GESTURE.SELF: "$You() $conj(kiss) $pron(yourself).",
+    },
+    "nag": {
+        GESTURE.DEFAULT: "$You() $conj(nag) everyone.",
+        GESTURE.TARGET: "$You() $conj(nag) $you(target).",
+        GESTURE.SELF: "$You() $conj(nag) $pron(yourself).",
     },
     "nod": {
         GESTURE.DEFAULT: "$You() $conj(nod).",
@@ -74,6 +119,11 @@ _GESTURES = {
         GESTURE.DEFAULT: "$You() $conj(point).",
         GESTURE.TARGET: "$You() $conj(point) at $you(target).",
         GESTURE.SELF: "$You() $conj(point) at $pron(yourself).",
+    },
+    "poke": {
+        GESTURE.DEFAULT: "$You() $conj(poke) yourself.",
+        GESTURE.TARGET: "$You() $conj(poke) $you(target).",
+        GESTURE.SELF: "$You() $conj(poke) $pron(yourself).",
     },
     "pray": {
         GESTURE.DEFAULT: "$You() $conj(pray).",
@@ -90,20 +140,40 @@ _GESTURES = {
         GESTURE.TARGET: "$You() $conj(shake) your head at $you(target).",
         GESTURE.SELF: "$You() $conj(shake) your head at $pron(yourself).",
     },
+    "slap": {
+        GESTURE.DEFAULT: "$You() $conj(slap) yourself.",
+        GESTURE.TARGET: "$You() $conj(slap) $you(target).",
+        GESTURE.SELF: "$You() $conj(slap) $pron(yourself).",
+    },
     "snap": {
         GESTURE.DEFAULT: "$You() $conj(snap) your fingers.",
         GESTURE.TARGET: "$You() $conj(snap) your fingers at $you(target).",
         GESTURE.SELF: "$You() $conj(snap) your fingers at $pron(yourself).",
+    },
+    "snuggle": {
+        GESTURE.DEFAULT: "$You() $conj(snuggle) yourself.",
+        GESTURE.TARGET: "$You() $conj(snuggle) $you(target).",
+        GESTURE.SELF: "$You() $conj(snuggle) $pron(yourself).",
     },
     "spin": {
         GESTURE.DEFAULT: "$You() $conj(spin) around.",
         GESTURE.TARGET: "$You() $conj(spin) around $you(target).",
         GESTURE.SELF: "$You() $conj(spin) around $pron(yourself), marveling at $pron(your) own magnificence.",
     },
+    "squeeze": {
+        GESTURE.DEFAULT: "$You() $conj(squeeze) yourself.",
+        GESTURE.TARGET: "$You() $conj(squeeze) $you(target).",
+        GESTURE.SELF: "$You() $conj(squeeze) $pron(yourself).",
+    },
     "thank": {
         GESTURE.DEFAULT: "$You() $conj(thank) everyone.",
         GESTURE.TARGET: "$You() $conj(thank) $you(target).",
         GESTURE.SELF: "$You() $conj(thank) $pron(yourself).",
+    },
+    "tickle": {
+        GESTURE.DEFAULT: "$You() $conj(tickle) yourself.",
+        GESTURE.TARGET: "$You() $conj(tickle) $you(target).",
+        GESTURE.SELF: "$You() $conj(tickle) $pron(yourself).",
     },
     "wait": {
         GESTURE.DEFAULT: "$You() $conj(wait).",
@@ -119,6 +189,11 @@ _GESTURES = {
         GESTURE.DEFAULT: "$You() $conj(welcome) everyone.",
         GESTURE.TARGET: "$You() $conj(welcome) $you(target).",
         GESTURE.SELF: "$You() $conj(welcome) $pron(yourself).",
+    },
+    "wink": {
+        GESTURE.DEFAULT: "$You() $conj(wink).",
+        GESTURE.TARGET: "$You() $conj(wink) at $you(target).",
+        GESTURE.SELF: "$You() $conj(wink) at $pron(yourself).",
     },
 }
 
