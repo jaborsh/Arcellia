162ae318de8e0e334e087f2740aff9544ca3581f
Jake <jaborsh@users.noreply.github.com>
2024-10-16 03:07:58 -0600

    Removing unused Solara.

--- world/zones/ooc/map.py
+++ world/zones/ooc/map.py
@@ -1,19 +1,11 @@
 from world.zones.ooc.room_prototypes import ROOM_PROTOTYPES
 
 OOC_MAP = r"""
- + 0 1 2 3 4
-
- 4      
-    
- 3 
-
- 2 
- 
- 1 
+ + 0
      
  0 #
 
- + 0 1 2 3 4
+ + 0
 """
 
 
--- world/zones/ooc/room_prototypes.py
+++ world/zones/ooc/room_prototypes.py
@@ -12,14 +12,5 @@ ROOM_PROTOTYPES = {
             A large hearth dominates one corner, but the flames that leap within it seem less concerned with warmth and more with casting a gentle, reassuring glow. In its light, a long, polished table gleams, cluttered with scattered remnants of discussions past - scrolls half-unfurled, quills long since abandoned, and goblets untouched save for a sip or two of some honeyed elixir.
             """
         ),
-        # "senses": {
-        #     "feel": "A frigid draft meanders through the space, carrying the unwelcome embrace of the grave.",
-        #     "smell": "The brine of the ocean mingles with the melancholy perfume of untreated wood and forlorn dreams.",
-        #     "sound": "The muted groans of the ship's hull, a morose lullaby to the departed, fills the eerie silence.",
-        #     "taste": "The air bears the faint brine of tears unwept, a taste of salt upon the tongue.",
-        # },
-        # "details": {
-        #     "bunks": "A singular bunk stands apart, of furnishing sparse and unadorned; it is here the sole waking soul finds themselves, untethered from death's silent banquet."
-        # },
     },
 }
